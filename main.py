ba965a69f28e13da8381da75bffcbe638f659098
prasetyo21 <prasetyobu21@gmail.com>
2021-12-24 11:04:45 +0700

    Update flow

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class_name = ['borobudur', 'prambanan', 'stupa_borobudur']
 
 
 @app.route('/ontology', methods=['GET'])
-def ontology2():
+def ontology():
     ssl._create_default_https_context = ssl._create_unverified_context
     sparql = SPARQLWrapper("http://localhost:8890/sparql")
     sparql.setQuery("""
@@ -37,7 +37,8 @@ def ontology2():
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    image = 'D:\Serius\Coding\DWIPA\python_api\model\img-testing.jpg'
+    # image = 'D:\Serius\Coding\DWIPA\py_model_test\model\img-testing.jpg'
+    image = 'D:\Serius\Coding\DWIPA\py_model_test\model\prambanan-testing.jpg'
     img = keras.preprocessing.image.load_img(image, target_size=(244, 244))
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
@@ -47,12 +48,12 @@ def predict():
     score = tf.nn.softmax(pred[0])
     result = class_name[np.argmax(score)]
     result2 = 100 * np.max(score)
-    prediction = {'Class': result, 'Score': result2}
-    return jsonify(prediction)
+    prediction = {'Class': result,
+                  'Score': result2}
+
+    query_result = ontology(result)
+    return query_result
 
 
 if __name__ == '__main__':
-    app.run(port=7777, debug=True)
-    # predict()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host="0.0.0.0", port=7777)
