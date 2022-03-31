c1d00fcacee8356eead971d7e118161c03e539f5
prasetyobu21 <prasetyobu21@gmail.com>
2022-03-31 04:09:08 +0000

    Server backup

--- main.py
+++ main.py
@@ -14,11 +14,56 @@ from tensorflow import keras
 import PIL
 from PIL import Image
 
+
+
 app = Flask(__name__)
 
 model = load_model('model/dwipa_model3.h5')
 class_name = ['borobudur', 'prambanan', 'stupa_borobudur']
 
+def predictImage():
+    image = request.files['file']
+    image = Image.open(image)
+    image = np.asarray(image.resize((244, 244)))
+    image = image.reshape(1, 244, 244, 3)
+
+    pred = model.predict(image)
+
+    score = tf.nn.softmax(pred[0])
+    result = class_name[np.argmax(score)]
+    result2 = 100 * np.max(score)
+
+    if result == "prambanan":
+        attraction = "PrambananTemple"
+    else:
+        attraction = "BorobudurTemple"
+
+    return attraction
+
+def sparql(query):
+    ssl._create_default_https_context = ssl._create_unverified_context
+    sparql = SPARQLWrapper("http://localhost:8890/sparql")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    data = {
+        'result': results["results"]
+    }
+    json_dump = jsonify(data)
+
+    return json_dump
+
+
+@app.route('/', methods=['GET'])
+def main():
+    data = {
+        'result': "This is Dwipa API",
+        'Author': "Prasetyo Budi Utomo"
+    }
+    json_dump = jsonify(data)
+
+    return json_dump
 
 @app.route('/ontology', methods=['GET'])
 def ontology():
@@ -39,6 +84,21 @@ def ontology():
 
     return json_dump
 
+@app.route('/ontologyQuery', methods=['POST'])
+def ontology2():
+    query = request.form['query']
+    ssl._create_default_https_context = ssl._create_unverified_context
+    sparql = SPARQLWrapper("http://localhost:8890/sparql")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    data = {
+        'result': results["results"]
+    }
+    json_dump = jsonify(data)
+
+    return json_dump
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -85,13 +145,159 @@ def predictO():
     score = tf.nn.softmax(pred[0])
     result = class_name[np.argmax(score)]
     result2 = 100 * np.max(score)
+
+    if result == "prambanan":
+        attraction = "PrambananTemple"
+    else:
+        attraction = "BorobudurTemple"
+
     print(score)
     print(result)
     print(result2)
     prediction = {'Class': result,
+                  'Attraction': attraction,
                   'Score': result2}
 
     return prediction
+
+@app.route('/img/searchPharmacy', methods=['POST'])
+def searchPharmacy():
+    attraction = predictImage()
+
+    query = """
+        PREFIX dwipa:<http://www.owl-ontologies.com/ETourismBali.owl/Touring#>
+        select ?Amenities
+        from <http://localhost:8890/dwipa>
+        where
+        {?Regency dwipa:hasAttraction dwipa:""" + attraction + """.
+        ?Amenities dwipa:hasPharmacyRegency ?Regency}
+        """
+
+    result = sparql(query)
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
+
+
+@app.route('/img/searchTransportation', methods=['POST'])
+def searctTransportation():
+    attraction = predictImage()
+
+    query = """
+        PREFIX dwipa:<http://www.owl-ontologies.com/ETourismBali.owl/Touring#>
+        select ?Amenities
+        from <http://localhost:8890/dwipa>
+        where
+        {?Regency dwipa:hasAttraction dwipa:""" + attraction + """.
+        ?Amenities dwipa:hasTransportationRegency ?Regency}
+        """
+
+    result = sparql(query)
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
+
+@app.route('/img/searchATM', methods=['POST'])
+def searchATM():
+    attraction = predictImage()
+
+    query = """
+        PREFIX dwipa:<http://www.owl-ontologies.com/ETourismBali.owl/Touring#>
+        select ?Amenities
+        from <http://localhost:8890/dwipa>
+        where
+        {?Regency dwipa:hasAttraction dwipa:""" + attraction + """.
+        ?Amenities dwipa:hasATMREgency ?Regency}
+        """
+
+    result = sparql(query)
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
+
+
+@app.route('/img/searchRestaurant', methods=['POST'])
+def searchRestaurant():
+    attraction = predictImage()
+
+    query = """
+        PREFIX dwipa:<http://www.owl-ontologies.com/ETourismBali.owl/Touring#>
+        select ?Amenities
+        from <http://localhost:8890/dwipa>
+        where
+        {?Regency dwipa:hasAttraction dwipa:""" + attraction + """.
+        ?Amenities dwipa:hasRestaurantRegency ?Regency}
+        """
+
+    result = sparql(query)
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
+
+
+@app.route('/txt/searchPharmacy', methods=['POST'])
+def searchPharmacyTxt():
+    attraction = request.form['attraction']
+
+    query = """
+        PREFIX dwipa:<http://www.owl-ontologies.com/ETourismBali.owl/Touring#>
+        select ?Amenities
+        from <http://localhost:8890/dwipa>
+        where
+        {?Regency dwipa:hasAttraction dwipa:""" + attraction + """.
+        ?Amenities dwipa:hasPharmacyRegency ?Regency}
+        """
+
+    result = sparql(query)
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
+
+@app.route('/txt/searchTransportation', methods=['POST'])
+def searchTrasportationTxt():
+    attraction = request.form['attraction']
+
+    query = """
+        PREFIX dwipa:<http://www.owl-ontologies.com/ETourismBali.owl/Touring#>
+        select ?Amenities
+        from <http://localhost:8890/dwipa>
+        where
+        {?Regency dwipa:hasAttraction dwipa:""" + attraction + """.
+        ?Amenities dwipa:hasTransportationRegency ?Regency}
+        """
+
+    result = sparql(query)
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
+
+@app.route('/txt/searchATM', methods=['POST'])
+def searchATMText():
+    attraction = request.form['attraction']
+
+    query = """
+        PREFIX dwipa:<http://www.owl-ontologies.com/ETourismBali.owl/Touring#>
+        select ?Amenities
+        from <http://localhost:8890/dwipa>
+        where
+        {?Regency dwipa:hasAttraction dwipa:""" + attraction + """.
+        ?Amenities dwipa:hasATMREgency ?Regency}
+        """
+
+    result = sparql(query)
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
+
+@app.route('/txt/searchRestaurant', methods=['POST'])
+def searchRestaurantTxt():
+    attraction = request.form['attraction']
+
+    query = """
+        PREFIX dwipa:<http://www.owl-ontologies.com/ETourismBali.owl/Touring#>
+        select ?Amenities
+        from <http://localhost:8890/dwipa>
+        where
+        {?Regency dwipa:hasAttraction dwipa:""" + attraction + """.
+        ?Amenities dwipa:hasRestaurantRegency ?Regency}
+        """
+
+    result = sparql(query)
+    result.headers.add("Access-Control-Allow-Origin", "*")
+    return result
 
 
 if __name__ == '__main__':
